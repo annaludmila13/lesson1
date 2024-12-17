592e83e1ffdc43a6aa5ef61883ec80a5383d315d
annaludmila13 <annaludmila10@gmail.com>
2024-12-17 19:36:12 +0300

    Add files via upload

---
+++ mobule_1_3.py
@@ -0,0 +1,14 @@
+name="Anna-Ludmila"
+print(name)
+age=63
+print(age)
+new_age=1
+age=age+new_age
+print(age)
+
+print(age+new_age)
+is_student=True
+print(is_student)
+
+my_string = "Hello, World!"
+print(dir(my_string))
